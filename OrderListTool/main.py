1dc684141d1ec3f93a23821cdfe58c40a92d1037
Matthias Schaefer <matthias-schaefer@freenet.de>
2021-03-30 15:53:22 +0200

    Lizenzinformationen hinzugefügt (GPLv3)

--- OrderListTool/main.py
+++ OrderListTool/main.py
@@ -1,3 +1,20 @@
+# Copyright (C) 2021 Matthias Schaefer
+# 
+# This file is part of OrderListTool.
+# 
+# OrderListTool is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# OrderListTool is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with OrderListTool.  If not, see <http://www.gnu.org/licenses/>.
+
 # main window
 import sys
 import os
